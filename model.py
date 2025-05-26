65f11ea5d44302497e0e5aa6617856927ed40b60
Samathmika Kurinjivanan <41962201+samathmika@users.noreply.github.com>
2025-05-26 11:52:26 +0200

    Updated model.py

--- model.py
+++ model.py
@@ -59,111 +59,143 @@ class BiasTestModel(nn.Module):
         usr = self.usrEmb(inputs[-1])
         clsClassification = self.classifierSecTask(torch.cat((mmRepresentations[:,0],usr.squeeze()),dim=1))
         return clsClassification
-    
-class TransformerModel(nn.Module):
 
-    def __init__(self,nrHeads,nrLayers,feedforwardDim,dropout,gpuNr = 0):
-        super(TransformerModel,self).__init__()
+class TransformerModel(nn.Module):
+    def __init__(self, nrHeads, nrLayers, feedforwardDim, dropout, gpuNr=0,userembed=True):
+        super(TransformerModel, self).__init__()
         
-        ###Variables
+        # Variables
         self.feedforwardDim = feedforwardDim
         self.inputDimension = 768
         self.nrHeads = nrHeads
         self.nrLayers = nrLayers
         self.gpuNr = gpuNr
         self.dropout = dropout
-        self.device = torch.device('cuda:'+str(self.gpuNr) if torch.cuda.is_available() else 'cpu')
-        ###Layer definition
-        self.bert = BertForMaskedLM.from_pretrained("bert-base-uncased",output_hidden_states=True).to(self.device)
+        self.device = torch.device('cuda:' + str(self.gpuNr) if torch.cuda.is_available() else 'cpu')
+
+        # BERT backbone
+        self.bert = BertForMaskedLM.from_pretrained("bert-base-uncased", output_hidden_states=True).to(self.device)
         for params in self.bert.parameters():
             params.requires_grad = False
-        self.downsize = nn.Linear(2048,768).to(self.device)
+
+        # Linear downsize for image features
+        self.downsize = nn.Linear(2048, 768).to(self.device)
         torch.nn.init.xavier_uniform_(self.downsize.weight)
-        self.encoderLayer = nn.TransformerEncoderLayer(self.inputDimension,self.nrHeads,self.feedforwardDim,dropout=self.dropout,batch_first=True).to(self.device)
-        self.transformerEncoder = nn.TransformerEncoder(self.encoderLayer,self.nrLayers).to(self.device)
+
+        # Transformer encoder
+        self.encoderLayer = nn.TransformerEncoderLayer(self.inputDimension, self.nrHeads, self.feedforwardDim, dropout=self.dropout, batch_first=True).to(self.device)
+        self.transformerEncoder = nn.TransformerEncoder(self.encoderLayer, self.nrLayers).to(self.device)
         for x in range(self.nrLayers):
             torch.nn.init.xavier_uniform_(self.transformerEncoder.layers[x].linear1.weight)
             torch.nn.init.xavier_uniform_(self.transformerEncoder.layers[x].linear2.weight)
 
         self.activation = nn.ReLU().to(self.device)
-        self.usrEmb = nn.Embedding(106,16).to(self.device)
+
+        # User embedding
+        self.usrEmb = nn.Embedding(106, 16).to(self.device)
         torch.nn.init.xavier_uniform_(self.usrEmb.weight)
 
-        self.classifierSecTask = nn.Linear(self.inputDimension+16,3).to(self.device)
-        ###Version for Ablation - replace line 92 by 94
-        #self.classifierSecTask = nn.Linear(self.inputDimension,3).to(self.device)
+        # Classification head
+        self.classifierSecTask = nn.Linear(self.inputDimension + 16, 3).to(self.device)
         torch.nn.init.xavier_uniform_(self.classifierSecTask.weight)
+
+        # Toggle flag
+        self.use_user_embeddings = userembed
 
     def getDevice(self):
         return self.device
 
-    def forward(self,inputs):
-        #usr Embedding
-        usr = self.usrEmb(inputs[-1])
-        #img features
-        downsizedImgs = self.downsize(inputs[0])
-        downsizedImgs = self.activation(downsizedImgs)
-        #word features
-        wordFeatures = self.bert(**inputs[2])
-        wordFeatures = wordFeatures['hidden_states'][-1]
-        #build input
-        mmInput = torch.cat((wordFeatures,downsizedImgs),1)
-        encoderMask = torch.cat((inputs[3],inputs[1]),1)
-        mmRepresentations = self.transformerEncoder(mmInput,src_key_padding_mask=encoderMask)
-        clsClassification = self.classifierSecTask(torch.cat((mmRepresentations[:,0],usr.squeeze()),dim=1))
-        ###Version for Ablation - replace line 113 by 115
-        #clsClassification = self.classifierSecTask(mmRepresentations[:,0])
+    def forward(self, inputs):
+        # inputs = (imageFeatures, imageMask, bertInputs, bertMask, userIds)
+        user_ids = inputs[-1]
+        wordInputs = inputs[2]
+        imgFeatures = inputs[0]
+        imgMask = inputs[1]
+        bertMask = inputs[3]
+
+        downsizedImgs = self.activation(self.downsize(imgFeatures))
+        wordFeatures = self.bert(**wordInputs)['hidden_states'][-1]
+
+        mmInput = torch.cat((wordFeatures, downsizedImgs), dim=1)
+        encoderMask = torch.cat((bertMask, imgMask), dim=1)
+        mmRepresentations = self.transformerEncoder(mmInput, src_key_padding_mask=encoderMask)
+
+        if self.use_user_embeddings:
+            usr = self.usrEmb(user_ids).squeeze()
+            clsClassification = self.classifierSecTask(torch.cat((mmRepresentations[:, 0], usr), dim=1))
+        else:
+            clsClassification = self.classifierSecTask(torch.cat((mmRepresentations[:, 0], torch.zeros((mmRepresentations.shape[0], 16), device=self.device)), dim=1))
+
         return clsClassification
-    
+
+
 class LSTMSequence(nn.Module):
-    def __init__(self,embSize,gpuNr = 0):
-        super(LSTMSequence,self).__init__()
+    def __init__(self, embSize, gpuNr=0,userembed=True):
+        super(LSTMSequence, self).__init__()
         self.embSize = embSize
         self.gpuNr = gpuNr
 
-        self.device = torch.device('cuda:'+str(self.gpuNr) if torch.cuda.is_available() else 'cpu')
-        self.userEmbed = nn.Embedding(num_embeddings=107,embedding_dim=self.embSize).to(self.device)
+        self.device = torch.device('cuda:' + str(self.gpuNr) if torch.cuda.is_available() else 'cpu')
+
+        self.userEmbed = nn.Embedding(num_embeddings=107, embedding_dim=self.embSize).to(self.device)
         torch.nn.init.xavier_uniform_(self.userEmbed.weight)
-        self.embed = nn.Embedding(num_embeddings=837,embedding_dim=self.embSize,padding_idx=0).to(self.device)
+
+        self.embed = nn.Embedding(num_embeddings=837, embedding_dim=self.embSize, padding_idx=0).to(self.device)
         torch.nn.init.xavier_uniform_(self.embed.weight)
-        self.lstm = nn.LSTM(input_size=self.embSize,hidden_size=self.embSize).to(self.device)
-        self.classifier = nn.Linear(self.embSize,3).to(self.device)
+
+        self.lstm = nn.LSTM(input_size=self.embSize, hidden_size=self.embSize).to(self.device)
+
+        self.classifier = nn.Linear(self.embSize, 3).to(self.device)
         torch.nn.init.xavier_uniform_(self.classifier.weight)
-        self.representationTransformation = nn.Linear(self.embSize*2,self.embSize).to(self.device)
-        ###Version for Ablation - replace line 132 by 134
-        #self.representationTransformation = nn.Linear(self.embSize,self.embSize).to(self.device)
+
+        self.representationTransformation = nn.Linear(self.embSize * 2, self.embSize).to(self.device)
         torch.nn.init.xavier_uniform_(self.representationTransformation.weight)
+
         self.activation = nn.ReLU()
+
+        # New flag for controlling user embeddings
+        self.use_user_embeddings = userembed
 
     def getDevice(self):
         return self.device
 
-    def forward(self,inputs,lengths,users):
+    def forward(self, inputs, lengths, users):
         embeds = self.embed(inputs)
-        userEmb = self.userEmbed(users)
-        inputrepresentation = torch.cat((embeds,userEmb.expand(embeds.shape)),dim=2)
-        ###Version for Ablation - replace line 144 by 146
-        #inputrepresentation = embeds #without user
-        inputrepresentation = self.activation(self.representationTransformation(inputrepresentation))
-        packedInputs = rnn.pack_padded_sequence(inputrepresentation,lengths,enforce_sorted=False)
-        rnnouts, (hn,cn) = self.lstm(packedInputs,(userEmb,userEmb))
-        ###Version for Ablation - replace line 149 by 151
-        #rnnouts, (hn,cn) = self.lstm(packedInputs) #without user
+
+        if self.use_user_embeddings:
+            userEmb = self.userEmbed(users)
+            inputrepresentation = torch.cat((embeds, userEmb.expand(embeds.shape)), dim=2)
+            inputrepresentation = self.activation(self.representationTransformation(inputrepresentation))
+            packedInputs = rnn.pack_padded_sequence(inputrepresentation, lengths, enforce_sorted=False)
+            rnnouts, (hn, cn) = self.lstm(packedInputs, (userEmb, userEmb))
+        else:
+            inputrepresentation = embeds
+            packedInputs = rnn.pack_padded_sequence(inputrepresentation, lengths, enforce_sorted=False)
+            rnnouts, (hn, cn) = self.lstm(packedInputs)
+
         outs = self.classifier(cn[-1])
         return outs
-    
+
 class Ensemble(nn.Module):
-    def __init__(self,embSize,nrHeads,nrLayers,feedforwardDim,dropout,gpuNr = 0):
+    def __init__(self,embSize,nrHeads,nrLayers,feedforwardDim,dropout,gpuNr = 0,f=True,userembed=True):
         super(Ensemble,self).__init__()
-        self.sequenceModel = LSTMSequence(embSize,gpuNr)
-        self.featureModel = TransformerModel(nrHeads,nrLayers,feedforwardDim,dropout,gpuNr)
+        self.userembed=userembed
+        self.sequenceModel = LSTMSequence(embSize,gpuNr,self.userembed)
+        self.featureModel = TransformerModel(nrHeads,nrLayers,feedforwardDim,dropout,gpuNr,self.userembed)
         self.device = self.featureModel.device
         self.finalDecision = nn.Linear(6,3).to(self.device)
         self.dropout = nn.Dropout(0.05)
+        self.f=f
     
     def forward(self,seqIns,featIns):
         sequences,lengths,users = seqIns
         model1Outs = self.sequenceModel(sequences,lengths,users)
         model2Outs = self.featureModel(featIns)
-        outs = torch.add(model1Outs,model2Outs)/2
+        if self.f:
+            outs = torch.add(model1Outs,model2Outs)/2
+        else:
+            combined = torch.cat((model1Outs, model2Outs), dim=1)
+            outs = self.finalDecision(combined)
         return outs
+
+
